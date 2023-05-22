4e026287caee47abc8fc8211e3521382620cf4e0
Peter Kim <petertimmmybob113@gmail.com>
2023-05-22 01:21:14 -0500

    rough calculator template

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let firstNum;
+let operator;
+let secondNum;
+
+function operate(operator, firstNum, secondNum) {
+  if (operator === "+") {
+    return add(firstNum, secondNum);
+  }
+  if (operator === "-") {
+    return subtract(firstNum, secondNum);
+  }
+  if (operator === "*") {
+    return multiply(firstNum, secondNum);
+  }
+  if (operator === "/") {
+    return divide(firstNum, secondNum);
+  }
+}
