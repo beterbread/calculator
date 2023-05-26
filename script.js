c5d26a84f47ff24be067f4b05895779bcf37a2f5
Peter Kim <petertimmmybob113@gmail.com>
2023-05-26 00:08:13 -0500

    negative number functionality

--- script.js
+++ script.js
@@ -49,59 +49,66 @@ const topDisplay = document.querySelector('#top');
 const bottomDisplay = document.querySelector('#bottom');
 const buttons = document.querySelectorAll('.operand');
 buttons.forEach(button => {
-  button.addEventListener('click', function() {
-    if (isOperator(button.textContent) && !containsOperator(bottomDisplay.textContent)) {
-      if (bottomDisplay.textContent !== "") {
+  button.addEventListener('click', function () {
+    let arr = bottomDisplay.textContent.split(" ")
+    if (isOperator(button.textContent)) {
+      if (bottomDisplay.textContent === "" && button.textContent === "-") {
+        bottomDisplay.textContent +=  "-"; 
+      }
+      else if (arr.length === 3 && button.textContent === "-") {
+        bottomDisplay.textContent +=  "-";   
+      }
+      else if (bottomDisplay.textContent !== "" && arr.length !== 3) {
         bottomDisplay.textContent += (" " + button.textContent + " ");
-      }     
+      }
     }
     else if (!isOperator(button.textContent)) {
       if (button.textContent === ".") {
         let arr = bottomDisplay.textContent.split(" ")
         if (containsOperator(bottomDisplay.textContent)) {
           if (!arr[2].includes(".")) {
-            bottomDisplay.textContent += button.textContent;   
+            bottomDisplay.textContent += button.textContent;
           }
-        }  
+        }
         else if (!arr[0].includes(".")) {
-            bottomDisplay.textContent += button.textContent;      
+          bottomDisplay.textContent += button.textContent;
         }
       }
       else {
-        bottomDisplay.textContent += button.textContent; 
-      }  
-    }    
+        bottomDisplay.textContent += button.textContent;
+      }
+    }
   });
 });
 
 buttons.forEach(button => {
-  button.addEventListener('hover', function() {
+  button.addEventListener('hover', function () {
 
   })
 })
 
 const clearBtn = document.querySelector('#clear');
-clearBtn.addEventListener('click', function() {
-    topDisplay.textContent = "";
-    bottomDisplay.textContent = "";
+clearBtn.addEventListener('click', function () {
+  topDisplay.textContent = "";
+  bottomDisplay.textContent = "";
 });
 
 const deleteBtn = document.querySelector('#delete');
-deleteBtn.addEventListener('click', function() {
+deleteBtn.addEventListener('click', function () {
   let str = bottomDisplay.textContent;
   if (str.substring(str.length - 1, str.length) === " ") {
-    bottomDisplay.textContent = str.substring(0, str.length - 2); 
+    bottomDisplay.textContent = str.substring(0, str.length - 2);
   }
   else {
-    bottomDisplay.textContent = str.substring(0, str.length - 1);  
+    bottomDisplay.textContent = str.substring(0, str.length - 1);
   }
 });
 
 const equalBtn = document.querySelector('.equal');
-equalBtn.addEventListener('click', function() {
+equalBtn.addEventListener('click', function () {
   let arr = bottomDisplay.textContent.split(" ")
   if (arr.length === 3) {
     topDisplay.textContent = bottomDisplay.textContent + " =";
-    bottomDisplay.textContent = operate(arr[1], arr[0], arr[2]);  
+    bottomDisplay.textContent = operate(arr[1], arr[0], arr[2]);
   }
 });
